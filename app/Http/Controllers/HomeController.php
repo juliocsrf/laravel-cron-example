a4bf0282a555ebc2399368dc2ae06b3b0acca083
Julio Cesar <juliocsrmf@gmail.com>
2021-01-20 20:16:25 -0300

    ajuste home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Jobs\EveryMinuteJob;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $cron = new EveryMinuteJob();
-        $cron();
-        return response()->json(['success'=>true]);
+        return response()->json(['Hello'=>'World']);
     }
 }
