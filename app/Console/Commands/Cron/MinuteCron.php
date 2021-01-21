0c6f107fc8409e77d9426669bf270d98dd227e6b
Julio Cesar <juliocsrmf@gmail.com>
2021-01-20 21:42:46 -0300

    teste

--- app/Console/Commands/Cron/MinuteCron.php
+++ app/Console/Commands/Cron/MinuteCron.php
@@ -39,7 +39,7 @@ class MinuteCron extends Command
     public function handle()
     {
         $cron = new Cron();
-        $cron->every_minute = date('Y-m-d H:i:s', strtotime('+2 days'));
+        $cron->every_minute = date('Y-m-d H:i:s', strtotime('+10 days'));
         return $cron->save();
     }
 }
