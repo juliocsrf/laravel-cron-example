0619ce68bea63810bd111147e332f966b4ab5e42
Julio Cesar <juliocsrmf@gmail.com>
2021-01-20 21:36:22 -0300

    Teste Webhook Github

--- app/Console/Commands/Cron/MinuteCron.php
+++ app/Console/Commands/Cron/MinuteCron.php
@@ -39,7 +39,7 @@ class MinuteCron extends Command
     public function handle()
     {
         $cron = new Cron();
-        $cron->every_minute = date('Y-m-d H:i:s');
+        $cron->every_minute = date('Y-m-d H:i:s', strtotime('+2 days'));
         return $cron->save();
     }
 }
